022a2d35b98d266561d8230d0c1e24adc6a9c889
student-manokamna <145145414+student-manokamna@users.noreply.github.com>
2025-06-02 18:33:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
@@ -0,0 +1,19 @@
+/*Complete the function below
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){80
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+    public:
+    
+};
