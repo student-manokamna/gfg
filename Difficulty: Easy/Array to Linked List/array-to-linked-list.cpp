123eaef1a5593103f882e79ac418dfdf2ea76bdb
student-manokamna <145145414+student-manokamna@users.noreply.github.com>
2025-06-16 19:34:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -0,0 +1,38 @@
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        if(arr.size()==0)
+        return NULL;
+        Node*head= new Node(arr[0]);
+        Node*curr=head;
+        for(int i=1;i<arr.size();i++){
+            Node*temp=new Node(arr[i]);
+            curr->next=temp;
+            curr=temp;
+        }
+        return head;
+    }
+};
