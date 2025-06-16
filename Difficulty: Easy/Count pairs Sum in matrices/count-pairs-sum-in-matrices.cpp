f571ca2f92fbbd0d854d1836f99f0d0bdb5fcba3
student-manokamna <145145414+student-manokamna@users.noreply.github.com>
2025-06-16 23:41:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
+++ Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -2,21 +2,28 @@ class Solution {
   public:
     int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int x) {
         // code here
-        set<int>s1;
-        set<int>s2;
-        for(int i=0;i<mat1.size();i++){
-            for(int j=0;j<mat1.size();j++){
-            s1.insert(mat1[i][j]);
-            s2.insert(mat2[i][j]);
+        unordered_map<int, int> freq;
+    int n = mat1.size();
+
+    // Step 1: Store frequencies of mat2 elements
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            freq[mat2[i][j]]++;
         }
+    }
+
+    // Step 2: Traverse mat1 and check for complement in freq map
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int complement = x - mat1[i][j];
+            if (freq.find(complement) != freq.end()) {
+                count += freq[complement];
+            }
         }
-        int count=0;
-        
-        for(auto y: s1){
-            if(s2.find(x-y)!=s2.end())
-            count++;
-        }
-        return count;
+    }
+
+    return count;
         
     }
 };
