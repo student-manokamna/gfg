838521030ed3091192df461a5368f7947a3eff63
student-manokamna <145145414+student-manokamna@users.noreply.github.com>
2025-06-23 13:09:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-//  m-2 now using dp we do -> tabular m  as it is done after meorisation  
-//    as here in tabular we do -> definne dp then first we deal with base cases 
-//   base cases meanns we satrt fom here then we do recursiove calls
-//  and here we defne by dp[]... 
+//  m-3 using o(1) space means now it is after tabular to -> space optimisation
+
 
     int minCost(vector<int>& height) {
         int n = height.size();
-          vector<int>dp(n,-1);
-              dp[0]=0;
+          int prev2=0;
+          int prev=0;
               for(int i=1;i<n;i++){
-                   int left = dp[i-1]+ abs(height[i] - height[i - 1]);
+                   int left = prev+ abs(height[i] - height[i - 1]);
 
         int right = INT_MAX; // bcz when we have f(1) then left is defined as f(0) but no right na 
         if (i > 1)
-            right = dp[i-2] + abs(height[i] - height[i - 2]);
-
-dp[i]= min(left, right);
+            right = prev2 + abs(height[i] - height[i - 2]);
+int curr= min(left, right);
+prev2=prev;
+prev=curr;
               }
             
-      return dp[n-1];
+      return prev;
     }
 };
 
